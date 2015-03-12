bf9c5b8ffa7f7360542b6b06c8fea95ad3511b10
jflec415 <jflec415@hotmail.com>
2015-03-12 08:05:59 -0400

    first unit test finished

--- mainwindow.h
+++ mainwindow.h
@@ -24,17 +24,6 @@ private slots:
     void keyReleaseEvent(QKeyEvent*);
     void on_pbLoad_clicked();
     void levelButtonClicked();
-    /*
-    void on_bt1_clicked();
-    void on_bt2_clicked();
-    void on_bt3_clicked();
-    void on_bt4_clicked();
-    void on_bt5_clicked();
-    void on_bt6_clicked();
-    void on_bt7_clicked();
-    void on_bt8_clicked();
-    void on_bt9_clicked();
-    */
 };
 
 #endif // MAINWINDOW_H
