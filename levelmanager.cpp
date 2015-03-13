6fad50e35e1423510abfb903870beac97f90382f
jflec415 <jflec415@hotmail.com>
2015-03-12 21:13:38 -0400

    wrote fake code for saveHighScore(); this segment of code extracts the values in the HighScore.txt and inserts the new highscore into its proper place in the hiarchy.

--- levelmanager.cpp
+++ levelmanager.cpp
@@ -239,6 +239,7 @@ void LevelManager::saveFile() {
     rename("tempFile.txt","saveFile.txt");
 }
 
+//the current code only writes to the file. will build the file into high pile of all scores ever gained in games
 void LevelManager::saveHighScore() {
     ofstream os("HighScore.txt");
 
@@ -250,6 +251,64 @@ void LevelManager::saveHighScore() {
 
     //close file
     os.close();
+
+/* THIS CODE EXTRACTS VALUES, ORGANIZES THEM, AND THEN OUTPUTS THEM TO THE FILE
+
+ //WARINGING: THIS CODE ASSUMES VALUES IN THE HIGHSCORE FILE; WILL PROBABLY PUT IN FALSE VALUES FOR USER TO COMPETE AGAINST
+
+class
+{
+    QString name;
+    int HighScore;
+
+public: //getters and setters
+
+}
+
+vector<class> vec;
+char c[20];
+ifstream in("saveFile.txt");
+
+//load the contents of the file into a vector of comparing with new values
+for (int i = 0; i < 5; i++)
+{
+    class cl = new class();
+
+    in.getline(c, 20);
+
+    cl.setName(QString(c));
+
+    in.getline(c, 20);
+
+    cl.setHighScore(*c); //need one setter to take a pointer
+
+    vec.push_back(cl);
+}
+
+auto counter = vec.begin();
+class cl;
+
+//search through every element
+for (element : vec)
+{
+    //if an element is smaller, then create a new class
+    if(element.getName() < LevelManager::getUserName())
+    {
+        cl = new class();
+        cl.setName(LevelManager::getUserName());
+        cl.setHighScore(LevelManager::getHighScore()); // need another setter to take a simple int variable
+
+        //this line will explode if the userScore is higher than the highest score.
+        vec.insert(counter - 1, cl); //FIND A WAY TO INSERT CODE AT THE BEGINNING OF A VECTOR OR LEAVE THE FIRST ELEMENT OPEN SO THAT AN ELEMENT MAY BE INSERTED THERE.
+    }
+    ++counter;
+}
+
+//write the elements to a new HighScore.txt
+
+//destroy the vector and delete elements
+
+*/
 }
 
 //attempts to load a saved lastUnlockedLevel by comparing username to saveFile.txt
@@ -275,13 +334,7 @@ void LevelManager::loadFile() {
                 //since we found user name, then read the user's lastUnlockedLevel
                 fs.getline(a, 20);
 
-                //convert a to a number - how? this work?
-                //THIS code works in the VM but not my mack
-/*                s << a;
-                int i = stoi(s.str());
-                s.str("");    */
-
-                //REPLACEMENT code
+                //convert to number
                 int i = *a;
 
                 //load number into
